16b8209541e692c66472a4b1270c2e67c0068d95
Lim Suyeon <shycat4151@gmail.com>
2023-04-09 15:23:47 +0900

    React에서 리스트 렌더링하기 material-ui 반영

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Todo from './Todo';
+import {Paper, List}from "@material-ui/core";
 import './App.css';
 
 class App extends React.Component {
@@ -7,19 +8,24 @@ class App extends React.Component {
     super(props);     // 매개변수 pros 초기화
     this.state ={   // item 에 item.id, item.title, item.done 매개변수 이름과 값 할당
       items :[
-        {id:0, title:"Todo 1 ", done:false},
+        {id:0, title:"Todo 1 ", done:true},
         {id:1, title:"Todo 2 ", done:false},
       ],
     };
   }
   
   render(){
-    // 자바스크립트가 제공하는 map 함수를 이용해서 배열을 반복해 <Todo /> 컴포넌트를 여러개 생성한다.
-    var todoItems =this.state.items.map((item,idx)=>(
-      <Todo item={item} key={item.idx}/>
-    ));
+    var todoItems =this.state.items.length >0 &&(
+      <Paper style={{margin:16}}>
+        <List>
+        {this.state.items.map((item,idx)=>(
+        <Todo item={item} key={item.id}/>
+        ))}
+        </List>
+      </Paper>
 
-    // 생성된 컴포넌트 JSX를 리턴한다.
+    );
+
     return <div className="App">{todoItems}</div>;
   }
 }
